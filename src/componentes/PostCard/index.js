3ec77e96aeddac98e885d89d938a255deec9d0a9
silas-mariosa <silas.mariosa@hotmail.com>
2023-05-23 19:49:32 -0300

    #PageNotFound

--- src/componentes/PostCard/index.js
+++ src/componentes/PostCard/index.js
@@ -1,15 +1,18 @@
-import styles from "./Post.module.scss"
+import styles from "./Post.module.scss";
+import { Link } from "react-router-dom";
 
-export default function PostCard({post}) {
+export default function PostCard({ post }) {
   return (
-    <div className={styles.post}>
-    <img 
-        className={styles.capa}
-        src={`/assets/posts/${post.id}/capa.png`}
-        alt="imagem de capa"
-    />
-    <h2 className={styles.titulo}>{post.titulo}</h2>
-    <button className={styles.botaoLer}>Ler</button>
-    </div>
-  )
+    <Link to={`/posts/${post.id}`}>
+      <div className={styles.post}>
+        <img
+          className={styles.capa}
+          src={`/assets/posts/${post.id}/capa.png`}
+          alt="imagem de capa"
+        />
+        <h2 className={styles.titulo}>{post.titulo}</h2>
+        <button className={styles.botaoLer}>Ler</button>
+      </div>
+    </Link>
+  );
 }
--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,8 +1,29 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import posts from "json/posts.json";
+import PostModelo from "componentes/PostModelo";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import "./Post.css"
 
 export default function Post() {
-    const parametros = useParams()
+  const parametros = useParams();
+
+  const post = posts.find((post) => {
+    return post.id === +parametros.id;
+  });
+
+  if(!post){
+     return <h1>Post não encontrado...</h1>
+  }
   return (
-    <div>post</div>
-  )
+    <PostModelo
+      fotoCapa={`/assets/posts/${post.id}/capa.png`}
+      titulo={post.titulo}
+    >
+      <div className="post-markdown-container">
+        <ReactMarkdown>
+            {post.texto}
+        </ReactMarkdown>
+      </div>
+    </PostModelo>
+  );
 }
