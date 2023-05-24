b2d43cd3a9b6065000c10bc2fdbda96dc96d26f0
silas-mariosa <silas.mariosa@hotmail.com>
2023-05-24 15:42:51 -0300

    #Post Recomended - ScrollToTop

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -5,9 +5,16 @@ import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import "./Post.css";
 import NaoEncontrada from "paginas/NaoEncontrada";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import PostCard from "componentes/PostCard";
+import styles from "./Post.module.scss"
 
 export default function Post() {
   const parametros = useParams();
+
+  const postsSortidos = posts
+    .filter((post) => post.id !== +parametros.id)
+    .sort((a,b) => a - b)
+    .slice(0,4)  
 
   const post = posts.find((post) => {
     return post.id === +parametros.id;
@@ -25,6 +32,21 @@ export default function Post() {
         <div className="post-markdown-container">
           <ReactMarkdown>{post.texto}</ReactMarkdown>
         </div>
+      <h2 className={styles.tituloOutrosPosts }>
+        Outros posts que você pode gostar...
+      </h2>
+      <ul className={styles.postsRecomendados}>
+        {postsSortidos.map((post)=>(
+          <li key={post.id}>
+            <PostCard post={post}/>
+          </li>
+        ))}        
+      {/* {quatroPrimeirosDoArray.map((post) => (
+        <li>
+          <PostCard post={post} />
+        </li>
+      ))}       */}
+      </ul>
       </PostModelo>
     </PaginaPadrao>
   );
